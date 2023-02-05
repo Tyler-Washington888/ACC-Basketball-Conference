ba48d19fb1362893798a653ff2cb9399280dd4d2
Tyler washington <lilturtleneckbeats@gmail.com>
2023-02-05 03:03:22 -0500

    update getNumOfPlayers callback

--- backend/controllers/teamplayerController.js
+++ backend/controllers/teamplayerController.js
@@ -1,7 +1,7 @@
 const db = require("../config/db");
 
 //@desc GET number of players on each team
-//@route GET /teams/numofplayers
+//@route GET /numofplayers
 //@access Public 
 const getNumOfPlayers = (async (req, res) => {
     let sql = `SELECT Teams.TeamName AS Team, COUNT(Players.PlayerID) AS "Num of Players" FROM Players RIGHT JOIN Teams ON Players.TeamID = Teams.TeamID GROUP BY TeamName ORDER BY Teams.TeamName`;
@@ -9,7 +9,10 @@ const getNumOfPlayers = (async (req, res) => {
       if(err) throw err;
       res.status(200).json(teams)
     })
-  })
+})
+
+
+
 
   module.exports = {
     getNumOfPlayers
